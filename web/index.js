8b2d3a3cbb668a4e2f61c1c4adac6133e7f9469c
Richard Wong <ricwtk@gmail.com>
2017-12-29 15:54:29 +0800

    add basic ui for detail editting

--- web/index.js
+++ web/index.js
@@ -79,10 +79,15 @@ function updateFileContent(fileId) {
 }
 
 Vue.component("single-bookmark", {
-  props: ["bookmark"],
+  props: ["bookmark", "allCategories"],
   data: function () {
     return {
-      showDetails: false
+      showDetails: true
+    }
+  },
+  computed: {
+    notCat: function () {
+      return this.allCategories.filter(c => !this.bookmark.categories.includes(c));
     }
   },
   methods: {
@@ -96,12 +101,30 @@ Vue.component("single-bookmark", {
         this.$emit("error", "URL (" + this.bookmark.url + ") cannot be opened from script due to browser limitation");
       }
     },
-    removeBoomark: function () {
+    removeBookmark: function () {
       this.$emit("removebookmark", this.bookmark.url);
     },
     clickText: function (el) {
       this.$emit("clicktext", el.target.textContent);
-    }
+    },
+    monitorInput: function (el) {
+      let catEntry = el.target.value;
+      if (catEntry[catEntry.length-1] == ",") {
+        let newCat = catEntry.slice(0, catEntry.length-1)
+        this.addCat(newCat);
+        el.target.value = "";
+      }
+    },
+    enterCat: function (el) {
+      let catEntry = el.target.value;
+      this.addCat(catEntry);
+      el.target.value = "";
+    },
+    addCat: function (newCat) {
+      this.$emit("addcat", { url: this.bookmark.url, newCat: newCat });
+      this.bookmark.categories.push(newCat);
+      this.bookmark.categories = this.bookmark.categories.filter((s,i,a) => a.indexOf(s) == i);
+    },
   },
   template: `
     <div class="single-bookmark">
@@ -111,16 +134,40 @@ Vue.component("single-bookmark", {
           <div class="bm-title" @click="clickText">{{ bookmark.customTitle ? bookmark.customTitle : bookmark.title }}</div>
           <div class="bm-url" @click="clickText">{{ bookmark.url }}</div>
           <div class="actions-group">
+            <div class="fa fa-minus" @click="removeBookmark"></div>
             <div class="fa fa-external-link-square" @click="openBookmark"></div>
           </div>
         </div>
         <div class="fa fa-caret-down" @click="toggleDetails"></div>
       </div>
-      <!--<transition name="details-display">
-        <div class="tab-list" v-if="showTabList" ref="tabList">
-          <single-tab-detail v-for="(tab, tid) in window.tabs" :tab="tab" @removetab="removeTab(tid)"></single-tab-detail>
+      <div class="details" v-if="showDetails">
+        <div class="title-edit">
+          Custom title: <input type="text" v-model="bookmark.customTitle">
         </div>
-      </transition>-->
+        <div class="cat-edit">
+          <div class="cat-display">
+            Categories:
+            <span v-for="cat in bookmark.categories" class="tag">
+              {{ cat }}
+              <i class="fa fa-times" :data-tagname="cat"></i>
+            </span>
+          </div>
+          <div class="cat-input">
+            <i class="fa fa-plus"></i>
+            <input type="text" @input="monitorInput" @change="enterCat">
+          </div>
+          <div class="more-cat-display">
+          <span v-for="cat in notCat" class="tag">
+            {{ cat }}
+            <i class="tag-action fa fa-plus" :data-tagname="cat"></i>
+          </span>
+          </div>
+        </div>
+        <div class="desc-edit">
+          Description:
+          <textarea v-model="bookmark.description" rows="5">
+        </div>
+      </div>
     </div>
   `
 });
@@ -233,6 +280,11 @@ v_app = new Vue({
     message: "",
     messageType: "info"
   },
+  computed: {
+    allCategories: function () {
+      return Array.prototype.concat(...this.bookmarks.map(bm => bm.categories)).filter((s,i,a) => a.indexOf(s) == i).sort((a,b) => a.localeCompare(b));
+    }
+  },
   methods: {
     openSyncBookmarksHome: () => window.open("../", "_self"),
     clickOnAccount: function () {
@@ -277,6 +329,12 @@ v_app = new Vue({
       // setTimeout(() => {
       //   if (this.message == message) this.message = "";
       // }, 3000);
+    },
+    addCat: function (m) {
+      let tbm = this.bookmarks.find(bm => bm.url == m.url);
+      tbm.categories.push(m.newCat);
+      tbm.categories = tbm.categories.filter((s,i,a) => a.indexOf(s) == i);
+      getFileId().then(updateFileContent);
     }
   }
 })
