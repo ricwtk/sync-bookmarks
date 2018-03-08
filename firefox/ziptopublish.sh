f6fad99499dd2ecf7f5ff60dbb8eedd985c32d25
Richard Wong <ricwtk@gmail.com>
2018-03-08 17:07:29 +0800

    update to remove unsafe eval from content security policy

---
+++ firefox/ziptopublish.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm syncbookmarks.zip
+zip syncbookmarks.zip font-awesome-4.7.0/css/font-awesome.min.css font-awesome-4.7.0/fonts/*-webfont* icons/icon.png background.js manifest.json popup.css popup.html popup.js
