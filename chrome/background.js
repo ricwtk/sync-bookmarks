2ef2bcb05024942d71f260d7b30debe930b688a7
Unknown <ricwtk@gmail.com>
2018-04-10 14:18:49 +0800

    Update new logic in background.js and popup.js (chrome)
    to avoid bookmarks loss at rapid handling

--- chrome/background.js
+++ chrome/background.js
@@ -143,113 +143,161 @@ function connected(p) {
     console.log("From popup.js", m);
     let retM = {};
     let mKeys = Object.keys(m);
-    if (mKeys.includes("refresh")) return onLocalModified(false);
-    if (mKeys.includes("setLocal")) return updateLocal(m.setLocal, true);
-    if (mKeys.includes("add")) bookmarks.push(m.add);
-    if (mKeys.includes("remove")) bookmarks.splice(bookmarks.findIndex(bm => bm.url == m.remove), 1);
-    if (mKeys.includes("setSortFeature")) sortFeature = m.setSortFeature;
-    if (mKeys.includes("setSortOrder")) sortOrder = m.setSortOrder;
-    if (mKeys.includes("addCat")) {
-      let tbm = bookmarks.find(bm => bm.url == m.addCat.url);
-      tbm.categories.push(m.addCat.newCat);
-      tbm.categories = tbm.categories.filter((s,i,a) => a.indexOf(s) == i);
+    if (mKeys.includes("refresh")) {
+      sendToDataPort({
+        bookmarks: bookmarks,
+        sortFeature: sortFeature,
+        sortOrder: sortOrder,
+        useLocal: useLocal,
+        remoteAccount: remoteAccount
+      })
+      return getData(false).then(sendToDataPort);
     }
-    if (mKeys.includes("removeCat")) {
-      let tbm = bookmarks.find(bm => bm.url == m.removeCat.url);
-      let removeIdx = tbm.categories.indexOf(m.removeCat.removeCat);
-      if (removeIdx > -1) tbm.categories.splice(removeIdx, 1);
+    if (mKeys.includes("setLocal")) return updateLocal(m.setLocal, true).then(() => getData(false)).then(sendToDataPort);
+    if (["setSortFeature", "setSortOrder"].some( el => mKeys.includes(el) )) {
+      if (mKeys.includes("setSortFeature")) sortFeature = m.setSortFeature;
+      if (mKeys.includes("setSortOrder")) sortOrder = m.setSortOrder;
+      syncToStorage({
+        bookmarks: bookmarks,
+        sortFeature: sortFeature,
+        sortOrder: sortOrder
+      })
     }
-    if (mKeys.includes("changeCustomTitle")) {
-      let tbm = bookmarks.find(bm => bm.url == m.changeCustomTitle.url);
-      tbm.customTitle = m.changeCustomTitle.new;
-    }
-    if (mKeys.includes("changeDescription")) {
-      let tbm = bookmarks.find(bm => bm.url == m.changeDescription.url);
-      tbm.description = m.changeDescription.new;
-    }
-    dataPort.postMessage({
-      bookmarks: bookmarks,
-      sortFeature: sortFeature,
-      sortOrder: sortOrder,
-      useLocal: useLocal,
-      remoteAccount: remoteAccount
-    });
-    if (useLocal) {
-      chrome.storage.local.set({
-        "sync-bookmarks-data": {
-          bookmarks: bookmarks,
-          sortFeature: sortFeature,
-          sortOrder: sortOrder
-        }
-      });
-    } else {
-      gDriveGetFileId()
-        .then(file => gDriveSetContent(file, {
-          bookmarks: bookmarks,
-          sortFeature: sortFeature,
-          sortOrder: sortOrder
-        }));
-    }
+    if (["add", "remove", "addCat", "removeCat", "changeCustomTitle", "changeDescription"]
+      .some( el => mKeys.includes(el) )) {
+        return getData(false).then(d => {
+          if (mKeys.includes("add")) {
+            if (d.bookmarks.findIndex(bm => bm.url == m.add.url) < 0) 
+              d.bookmarks.push(m.add);
+          }
+          if (mKeys.includes("remove")) d.bookmarks.splice(d.bookmarks.findIndex(bm => bm.url == m.remove), 1);
+          if (mKeys.includes("addCat")) {
+            let tbm = d.bookmarks.find(bm => bm.url == m.addCat.url);
+            tbm.categories.push(m.addCat.newCat);
+            tbm.categories = tbm.categories.filter((s,i,a) => a.indexOf(s) == i);
+          }
+          if (mKeys.includes("removeCat")) {
+            let tbm = d.bookmarks.find(bm => bm.url == m.removeCat.url);
+            let removeIdx = tbm.categories.indexOf(m.removeCat.removeCat);
+            if (removeIdx > -1) tbm.categories.splice(removeIdx, 1);
+          }
+          if (mKeys.includes("changeCustomTitle")) {
+            let tbm = d.bookmarks.find(bm => bm.url == m.changeCustomTitle.url);
+            tbm.customTitle = m.changeCustomTitle.new;
+          }
+          if (mKeys.includes("changeDescription")) {
+            let tbm = d.bookmarks.find(bm => bm.url == m.changeDescription.url);
+            tbm.description = m.changeDescription.new;
+          }
+          return {
+            bookmarks: d.bookmarks,
+            sortFeature: d.sortFeature,
+            sortOrder: d.sortOrder,
+            useLocal: d.useLocal
+          }
+        }).then(d => {
+          bookmarks = d.bookmarks;
+          sortFeature = d.sortFeature;
+          sortOrder = d.sortOrder;
+          syncToStorage({
+            bookmarks: d.bookmarks,
+            sortFeature: d.sortFeature,
+            sortOrder: d.sortOrder
+          });
+          sendToDataPort(d);
+        });
+      }
   });
 }
 
 function updateLocal(newLocal, interactive) {
   useLocal = newLocal;
-  chrome.storage.local.set({
-    "sync-bookmarks-local-prefs": { "useLocal": useLocal }
+  return new Promise(resolve => {
+    chrome.storage.local.set({
+      "sync-bookmarks-local-prefs": { "useLocal": useLocal }
+    });
+    resolve();
   });
-  onLocalModified(interactive);
 }
 
-function onLocalModified(interactive) {
+function sendToDataPort(data) {
+  try {
+    if (dataPort) dataPort.postMessage(data);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function syncToStorage(data) {
   if (useLocal) {
-    chrome.storage.local.get("sync-bookmarks-data", res => {
-      let resContent = res["sync-bookmarks-data"];
-      if (resContent) {
-        let rcKeys = Object.keys(resContent);
-        bookmarks = rcKeys.includes("bookmarks") ? resContent.bookmarks : [];
-        sortFeature = rcKeys.includes("sortFeature") ? resContent.sortFeature : 0;
-        sortOrder = rcKeys.includes("sortOrder") ? resContent.sortOrder : false;
+    return chrome.storage.local.set({
+      "sync-bookmarks-data": data
+    });
+  } else {
+    gDriveGetFileId().then(file => gDriveSetContent(file, data));
+  }
+}
+
+function getData(interactive) {
+  if (useLocal) {
+    ACCESS_TOKEN = "";
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get("sync-bookmarks-data", res => {
+        let resContent = res["sync-bookmarks-data"];
+        if (resContent) {
+          let rcKeys = Object.keys(resContent);
+          bookmarks = rcKeys.includes("bookmarks") ? resContent.bookmarks : [];
+          sortFeature = rcKeys.includes("sortFeature") ? resContent.sortFeature : 0;
+          sortOrder = rcKeys.includes("sortOrder") ? resContent.sortOrder : false;
+        } else {
+          bookmarks = [];
+          sortFeature = 0;
+          sortOrder = false;
+        }
+        resolve({
+          bookmarks: bookmarks,
+          sortFeature: sortFeature,
+          sortOrder: sortOrder,
+          useLocal: useLocal
+        });
+      });
+    });
+  } else {
+    return new Promise((resolve, reject) => {
+      if (!ACCESS_TOKEN) {
+        chrome.identity.getAuthToken({
+          interactive: interactive
+        }, token => {
+          if (token == undefined) {
+            console.log("Not logged in to Google");
+            updateLocal(true, interactive).then(() => getData(false)).then(sendToDataPort);
+          } else {
+            console.log("Logged in to Google");
+            resolve(afterGoogleLogin(token));
+          }
+        });
       } else {
-        bookmarks = [];
-        sortFeature = 0;
-        sortOrder = false;
+        resolve();
       }
+    }).then(gDriveGetFileId)
+    .then(gDriveGetContent)
+    .then(resp => {
+      console.log(resp);
+      let rKeys = Object.keys(resp);
+      bookmarks = rKeys.includes("bookmarks") ? resp.bookmarks : [];
+      sortFeature = rKeys.includes("sortFeature") ? resp.sortFeature : 0;
+      sortOrder = rKeys.includes("sortOrder") ? resp.sortOrder : false;
       if (dataPort) dataPort.postMessage({
         bookmarks: bookmarks,
         sortFeature: sortFeature,
         sortOrder: sortOrder,
-        useLocal: useLocal
+        useLocal: useLocal,
+        remoteAccount: remoteAccount
       });
-    });
-  } else {
-    chrome.identity.getAuthToken({
-      interactive: interactive
-    }, token => {
-      if (token == undefined) {
-        console.log("Not logged in to Google");
-        updateLocal(true, interactive);
-      } else {
-        console.log("Logged in to Google");
-        afterGoogleLogin(token)
-          .then(gDriveGetFileId)
-          .then(gDriveGetContent)
-          .then(resp => {
-            console.log(resp);
-            let rKeys = Object.keys(resp);
-            bookmarks = rKeys.includes("bookmarks") ? resp.bookmarks : [];
-            sortFeature = rKeys.includes("sortFeature") ? resp.sortFeature : 0;
-            sortOrder = rKeys.includes("sortOrder") ? resp.sortOrder : false;
-            if (dataPort) dataPort.postMessage({
-              bookmarks: bookmarks,
-              sortFeature: sortFeature,
-              sortOrder: sortOrder,
-              useLocal: useLocal,
-              remoteAccount: remoteAccount
-            });
-            return resp;
-          });
-        }
+      return resp;
+    }, err => {
+      ACCESS_TOKEN = "";
+      return getData(interactive);
     });
   }
 }
@@ -261,5 +309,5 @@ chrome.storage.local.get("sync-bookmarks-local-prefs", res => {
   if (resContent)
     useLocal = Object.keys(resContent).includes("useLocal") ? resContent["useLocal"] : useLocal;
   console.log("useLocal", useLocal);
-  onLocalModified(false);
+  getData(false).then(sendToDataPort);
 });
